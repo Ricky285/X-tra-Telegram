804cf7bfd7dace5a234d3bf28823dea9cb2523dd
Ricky285 <57534629+Ricky285@users.noreply.github.com>
2019-11-26 15:23:39 +0530

    Update stickers.py

--- userbot/plugins/stickers.py
+++ userbot/plugins/stickers.py
@@ -45,7 +45,7 @@ async def _(event):
     me = borg.me
     userid = event.from_id
     packname = f"@ig_Ricky's spam pack Vol.2"
-    packshortname = f"Dark_Hacker_278"  # format: Uni_Borg_userid
+    packshortname = f"Ricky_Spam{userid}"  # format: Uni_Borg_userid
 
     is_a_s = is_it_animated_sticker(reply_message)
     file_ext_ns_ion = "@UniBorg_Sticker.png"
